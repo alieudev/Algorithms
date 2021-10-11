796612edea01c825ad099a13ef07bd91f1996fbf
Alieu Baldeh <alieu.dev@gmail.com>
2021-10-10 21:23:04 -0400

    lonely integer

---
+++ HackerRank/lonelyInteger.py
@@ -0,0 +1,45 @@
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    if len(a) == 1: 
+        return a[0]
+    
+    lonelyDict = {}
+    for num in a: 
+        if num in lonelyDict: 
+            lonelyDict[num] += 1 
+        else: 
+            lonelyDict[num] = 1
+    
+    for key, value in lonelyDict.items():
+        if lonelyDict[key] == 1: 
+            return key
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
