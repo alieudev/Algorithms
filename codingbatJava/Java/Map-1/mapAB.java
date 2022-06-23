e6a0f7c87102f36aafafd5d0c871ea87d5693fdd
Alieu Baldeh <alieu.dev@gmail.com>
2022-06-23 13:16:22 -0400

    map ab

---
+++ codingbatJava/Java/Map-1/mapAB.java
@@ -0,0 +1,9 @@
+public Map<String, String> mapAB(Map<String, String> map) {
+ if(map.containsKey("a") && map.containsKey("b")){
+   String ab = map.get("a") + map.get("b");
+   map.put("ab", ab);
+   return map;
+ }
+ return map;
+}
+
