4d165ee9b3f4c57deff1d2f40d0105b0860e3014
Alieu Baldeh <alieu.dev@gmail.com>
2022-06-23 13:13:02 -0400

    mapAB3

---
+++ codingbatJava/Java/Map-1/mapAB3.java
@@ -0,0 +1,13 @@
+public Map<String, String> mapAB3(Map<String, String> map) {
+  if(map.containsKey("a") && map.containsKey("b")){
+    return map;
+  } else if (map.containsKey("a")){
+    map.put("b", map.get("a"));
+    return map;
+  } else if (map.containsKey("b")){
+    map.put("a", map.get("b"));
+    return map;
+  }
+  return map;
+}
+
