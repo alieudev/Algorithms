bf3bd46536580fdd36c1349c4ea5f365940cd9ef
Alieu Baldeh <alieu.dev@gmail.com>
2022-07-21 19:34:05 -0400

    caught speeding

---
+++ Codingbat/Python/String-2/caught_speeding.py
@@ -0,0 +1,18 @@
+def caught_speeding(speed, is_birthday):
+  if is_birthday: 
+    if speed > 85: 
+      return 2 
+    elif speed > 65 and speed <= 85 : 
+      return 1 
+    else: 
+      return 0
+  elif not is_birthday: 
+    if speed > 80: 
+      return 2 
+    if speed > 60 and speed <= 80: 
+      return 1 
+    else: 
+      return 0 
+    
+    
+
