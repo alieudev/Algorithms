4fe11887ebc3b1cf944dd455d8e372ced12d79e1
Alieu Baldeh <alieu.dev@gmail.com>
2022-07-22 17:10:26 -0400

    make bricks

--- Codingbat/Java/Logic-2/make_bricks.py
+++ Codingbat/Java/Logic-2/make_bricks.py
@@ -1,8 +1,5 @@
-def make_bricks(small, big, goal):
-  if goal > small + big * 5: 
-    return False 
-  
-  if goal % 5 > small: 
-    return False 
-  
-  return True
+public boolean makeBricks(int small, int big, int goal) {
+  if (big * 5 + small < goal) return false;
+  if (goal % 5 > small) return false;
+  return true;
+}
