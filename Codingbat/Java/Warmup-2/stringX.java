e90f1c9a87c891a5780d78d20add98cc1c440da1
Alieu Baldeh <alieu.dev@gmail.com>
2022-07-25 12:15:05 -0400

    stringX

---
+++ Codingbat/Java/Warmup-2/stringX.java
@@ -0,0 +1,11 @@
+public String stringX(String str) {
+  String result = "";
+  for (int i=0; i<str.length(); i++) {
+    // Only append the char if it is not the "x" case
+    if (!(i > 0 && i < (str.length()-1) && str.substring(i, i+1).equals("x"))) {
+      result = result + str.substring(i, i+1); // Could use str.charAt(i) here
+    }
+  }
+  return result;
+}
+
